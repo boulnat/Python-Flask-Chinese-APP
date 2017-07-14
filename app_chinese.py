c37868c9b4b33979a4bd3111d45f4546af16d492
boulnat-PC\boulnat <boulet_nathan@hotmail.com>
2017-07-14 22:00:58 +0200

    Initiales
    getting the information back from the form into the answer

--- app_chinese.py
+++ app_chinese.py
@@ -1,66 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Boulet Nathan
 
-This is a temporary script file.
 """
-from flask import Flask, flash, render_template, request, session
+from flask import Flask, flash, render_template, request, session, redirect, url_for
 import os, random
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
 
-tab=[]
-
 @app.route("/")
-def main(name="Unknown"):
-    if not session.get("logged_in"):
-        return render_template("login.html")
-    else:
-        return render_template('index.html', name=name)
-
-@app.route("/login", methods=["POST"])
-def login():
-    if request.method == 'POST':
-        if request.form["username"] == "boulnat" or request.form["password"] == "boulnat":
-            session["logged_in"] = True
-            loggedInName = "boulnat"
-        else:
-             print ("error")
-        return main(name=loggedInName)          
-
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return main()
-
-@app.route('/initiales', methods=['GET', 'POST'])
+def main():
+    return render_template("index.html")
+    
+@app.route('/initiales', methods=['POST','GET'])
 def initiales(valeur=None, result=None, vue=True):
-    tab=random.sample(range(100),4)
-    count=len(tab)
-    tabResult=[]
-    i=0
+    initiale_q=None
+    initiale_q=random.sample(range(100),2)      #initialise des valeurs pour l'exercices
+    string_value =  []
+    i=1
     if request.method == 'POST':
-        while i < count-1:
-            if request.form[str(tab[i])] == tab[i]:
-                tabResult.append("ok")
-            else:
-                tabResult.append(goodRequest)
-            i+=1
-        return render_template("initiales.html",result=tabResult,vue=False) 
-    return render_template('initiales.html',valeur=tab,vue=True)
+        #while i <= len(initiale_q):
+        #for v in request.form:
+        #    for a in v:
+        #        key, ans = v, a
+        #        string_value[key] = ans
+        string_value=(request.form)
+        #    i=i+1
+        #return redirect(url_for('answers',result=string_value))
+        return render_template("answers.html",result=string_value)
 
-@app.route('/finales')
-def finales():
-    return render_template('finales.html')
+    return render_template('initiales.html',valeur=initiale_q)
 
-@app.route('/tons')
-def tons():
-    return render_template('tons.html')
-
-@app.route('/statistiques')
-def statistiques():
-    return render_template('statistiques.html')
+@app.route("/answers")
+def answers():
+    return render_template("answers.html")
 
 if __name__ == "__main__":
     app.run(debug=True)    
+

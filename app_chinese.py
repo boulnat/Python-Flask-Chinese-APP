ec0428cf1947e8776178d3cb58f0926b67c14f68
boulnat-PC\boulnat <boulet_nathan@hotmail.com>
2017-07-21 00:44:41 +0200

    (initiales) récuper les reponese correctement avec l'audio

--- app_chinese.py
+++ app_chinese.py
@@ -25,20 +25,18 @@ class pinyin:
     def getListPinyin(self, size):
         return [tablePinyin for tablePinyin in self.listPinyin[0:size]]
 
+    def getSoundPathCorrection(self, tableSound):
+        tmpTab={}
+        for sound in tableSound:
+            if sound != "":
+                try:
+                     tmpTab.update({sound : os.path.join(self.path, sound + self.extension)})
+                except:
+                    print("error no sound found for" + os.path.join(self.path, sound + self.extension))
+        return tmpTab
+
     def getSoundPath(self, tableSound):
         return [os.path.join(self.path, sound + self.extension) for sound in tableSound]
-
-class initialesQuestions:
-    def __init__(self,tabInitiales):
-        self.tabInitiales=tabInitiales
-    def showInitiales(self):
-        tmpTab=[]
-        for valueInitiale in self.tabInitiales:
-            if valueInitiale[0:2] in ("zh","ch","sh"): #détermine si le mot commence par des initiales composés
-                tmpTab.append(valueInitiale[2:]) #selection sans les 2 premiers caractere
-            else:
-                tmpTab.append(valueInitiale[1:]) #selection sans le 1er caractere
-        return tmpTab
     
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
@@ -50,7 +48,7 @@ def main():
 @app.route('/initiales', methods=['POST','GET'])
 def initiales(valeur=None, result=None, vue=True,musicFiles=None):
     bonne_reponse = 0
-    NUM_OF_EX = 2
+    NUM_OF_EX = 5
     string_value =  []
     index=0
 
@@ -58,27 +56,24 @@ def initiales(valeur=None, result=None, vue=True,musicFiles=None):
     myPinyinList = myPinyin.getRandomListPinyin(NUM_OF_EX)
     myPinyinPath = myPinyin.getSoundPath(myPinyinList)
     myInitiales = myPinyinList
-    
-    #####
-    
+
     if request.method == 'POST':
-        print(request.form.to_dict())
-        print(myPinyinList)
-        while index < len(myPinyinList):
-            if myPinyinList[index] == request.form.to_dict(str(myPinyinList[index])):
+        myPinyinList = []
+        myPinyinPath = []
+        for question in request.form.to_dict():
+            myPinyinList.append(request.form.to_dict()[question])
+            myPinyinList.append(question)
+
+            if request.form.to_dict()[question] == question:
                 bonne_reponse+=1 #increment des bonnes répones pour le score final
-            else:
-                print("ok")
             index+=1
             string_value=(request.form)
-        print(bonne_reponse)
-        return render_template("answers.html",result=request.form)
-       
+        myPinyinPath = myPinyin.getSoundPathCorrection(myPinyinList)
+        return render_template("answers.html",corection=request.form,result=bonne_reponse,musicFiles=myPinyinPath)    
     return render_template('initiales.html',valeur=myInitiales,musicFiles=myPinyinPath)
 
 @app.route("/answers")
-def answers():
-            
+def answers(result=None):
     return render_template("answers.html")
 
 if __name__ == "__main__":
